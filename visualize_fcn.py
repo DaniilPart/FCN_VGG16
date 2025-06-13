0a4181c1b318ce40f8bd8ace2191c0cd69d174dc
Daniil <Gerffins606@gmail.com>
2025-06-13 11:13:05 +0200

    update visualize FCN

--- visualize_fcn.py
+++ visualize_fcn.py
@@ -8,8 +8,10 @@ num_classes = 2
 
 inputs = Input(shape=input_shape, name='input_image')
 
-# Симуляция выходов энкодера VGG16
-layer3_out = Conv2D(256, (3,3), padding='same', activation='relu', name='vgg_layer3_out')(inputs)
+block1_out = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_out')(inputs)
+block2_out = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_out')(block1_out)
+
+layer3_out = Conv2D(256, (3,3), activation='relu', padding='same', name='vgg_layer3_out')(block2_out)
 layer4_out = Conv2D(512, (3,3), padding='same', activation='relu', strides=(2,2), name='vgg_layer4_out')(layer3_out)
 layer7_out = Conv2D(4096, (7,7), padding='same', activation='relu', strides=(2,2), name='vgg_layer7_out')(layer4_out)
 
@@ -24,5 +26,9 @@ output = Conv2DTranspose(num_classes, (16,16), strides=(8,8), padding='same', na
 
 model = Model(inputs=inputs, outputs=output, name='FCN_VGG16')
 
-plot_model(model, to_file='fcn_vgg16_architecture.png', show_shapes=True, show_layer_names=True)
-print('Схема сохранена в fcn_vgg16_architecture.png')
+plot_model(model, to_file='extended_architecture.png', show_shapes=True, show_layer_names=True)
+print("pic in extended_architecture.png")
+
+print("\nAll layers in model:")
+for layer in model.layers:
+    print(f"Layer: {layer.name}, Shape input: {layer.input_shape}, Shape output: {layer.output_shape}")
